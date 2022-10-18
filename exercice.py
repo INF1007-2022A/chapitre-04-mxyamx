7186445eaadb89373f98e926184d9395796750f0
maya <aitmayamanel@gmail.com>
2022-10-18 10:17:49 -0400

    update

--- exercice.py
+++ exercice.py
@@ -1,50 +1,50 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
-#Vérifier si le nombre de caractères d’une chaîne de caractères est pair
-mon_nombre = int(input("Entrez un nombre: "))
-if (mon_nombre % 2) == 0:
-   print(str(mon_nombre) + " est Paire")
-else:
-   print(str(mon_nombre)  + " est impair")
-
-#Supprimer le 3ème caractère d’une chaîne de caractères
-chaine = input('entrez une chaine de charactere')
-def remove_third_char(string: str) -> str:
-    return string[:2] + string[3:]
-print(chaine)
-
-
-
-
 
 def is_even_len(string: str) -> bool:
+    return len(string) % 2 == 0
     pass
 
 
-def remove_third_char(string: str) -> str:
-    pass
+# def remove_third_char(string: str) -> str:
+#     pass
+#
+#
+# def replace_char(string: str, old_char: str, new_char: str) -> str:
+#     pass
+#
+#
+# def get_number_of_char(string: str, char: str) -> int:
+#     pass
+#
+#
+# def get_number_of_words(sentence: str, word: str) -> int:
+#     pass
+#
+#
+def main() -> None:
+    chaine = "Bonjour!"
+    if is_even_len(chaine):
+        print(f"Le nombre de caractère dans la chaine {chaine} est pair")
+    else:
+        print(f"Le nombre de caractère dans la chaine {chaine} est impair")
 
-
-def replace_char(string: str, old_char: str, new_char: str) -> str:
-
-
-    pass
-
-
-def get_number_of_char(string: str, char: str) -> int:
-    # Renvoyer le nombre d’occurrences d’un caractère dans une chaîne de caractères, sans utiliser de fonctions avancées
-    compteur = 0
-    for i in range (0, len(string)):
-        if string[i] == char:
-            compteur += 1
-    return compteur
-print(f"Le nb d'occurences de l dans hello est :{get_number_of_char(chaine, 'l')}")
-
-
-
-def get_number_of_words(sentence: str, word: str) -> int:
-    pass
+#     chaine = "salut monde!"
+#     print(f"On supprime le 3e caratère dans la chaine: {chaine}. Résultat : {remove_third_char(chaine)}")
+#
+#     chaine = "hello world!"
+#     print(
+#         f"On remplace le caratère w par le caractère z dans la chaine: {chaine}. Résultat : {replace_char(chaine, 'w', 'z')}")
+#
+#     print(f"Le nombre d'occurrence de l dans hello est : {get_number_of_char(chaine, 'l')}")
+#
+#     chaine = "Baby shark doo doo doo doo doo doo"
+#     print(f"L'occurence du mot doo dans la chaine {chaine} est: {get_number_of_words(chaine, 'doo')}")
+#
+#
+if __name__ == '__main__':
+    main()
 
 
 
